4972a02f9f96f401e24a1f23ef41a2a67700fa30
buonzz <buonzz@gmail.com>
2016-04-22 19:33:31 +0800

    removed mongodb

--- provision.sh
+++ provision.sh
@@ -28,14 +28,3 @@ sudo apt-get install npm -y
 sudo apt-get install git -y
 git clone git://github.com/n1k0/casperjs.git
 sudo ln -sf `pwd`/casperjs/bin/casperjs /usr/local/bin/casperjs
-
-
-#setup MongoDB
-
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
-sudo apt-get update
-sudo apt-get install -y mongodb-org
-
-sudo iptables -I INPUT -p tcp --dport 27017 -j ACCEPT
-sudo iptables-save
